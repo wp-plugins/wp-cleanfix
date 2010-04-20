983f988758bf8e6ee89b64535132f6f5fcad8c33
gfazioli <gfazioli@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-20 12:14:15 +0000

    = 1.2.0 = * Add Show Post/Page title in Combo Menu for Post Revision/Trash * Fix localization files
    git-svn-id: https://plugins.svn.wordpress.org/wp-cleanfix/tags/1.2.0@230947 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ module/database_view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Database management
+ *
+ * @package         wp-cleanfix
+ * @subpackage      database_view
+ * @author          =undo= <g.fazioli@saidmade.com>
+ * @copyright       Copyright (C) 2010 Saidmade Srl
+ *
+ */
+?>
+
+<table class="widefat wp-cleanfix" width="100%" cellpadding="4" cellspacing="0">
+    <thead>
+        <tr>
+            <th scope="col"><?php _e('Refresh', 'wp-cleanfix') ?></th>
+            <th scope="col"><?php _e('Action', 'wp-cleanfix') ?></th>
+            <th width="100%" scope="col"><?php _e('Status', 'wp-cleanfix') ?></th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr scope="row">
+            <td><?php $this->button_refresh('buttonDatabaseOptimizeTableRefresh') ?></td>
+            
+            <td>
+                <strong><?php _e('Tables optimize', 'wp-cleanfix') ?></strong>
+            </td>
+            <td>
+                <div id="database-optimize">
+                    <?php $WPCLEANFIX_DATABASE->checkTables() ?>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
---
+++ module/info.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Display all usefull server and envirorment informations
+ *
+ * @package         wp-cleanfix
+ * @subpackage      info
+ * @author          =undo= <g.fazioli@saidmade.com>
+ * @copyright       Copyright (C) 2010 Saidmade Srl
+ * @version         1.0.0
+ */
+
+?>
+<p><strong style="color:red"><?php _e('WARNING! SOME ACTION IS POTENTIALLY DANGEROUS FOR YOUR DATABASE INTEGRETY. PLEASE MAKE A DATABASE\'S BACKUP BEFORE EXECUTE ANY DELETING OR ANY ACTION. ALL DELETING OPERATIONS ARE IRREVERSIBLE!', 'wp-cleanfix') ?></strong></p>
